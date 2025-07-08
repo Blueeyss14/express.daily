165c0911a889f6e6a1968996c080062c10aec187
unknown <delkanoberutu8@gmail.com>
2025-07-09 00:35:30 +0700

    get method

--- first/src/index.mjs
+++ first/src/index.mjs
@@ -1,8 +1,23 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (request, response) => {
+    response.status(201).send({msg: 'hello'});
+});
+
+const users = [
+    { id: 1, username: "orang" },
+    { id: 2, username: "orang" },
+    { id: 3, username: "orang" },
+    { id: 4, username: "orang" },
+];
+
+app.get("/api/users/", (request, response,) => {
+    response.send(users);
+})
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
